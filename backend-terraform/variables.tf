09c032b4f03809f8acba7b866c281b3b94c2e29f
Tu Nguyen <im2nguyen@gmail.com>
2022-02-03 03:26:45 -0800

    Update TF variables

--- backend-terraform/variables.tf
+++ backend-terraform/variables.tf
@@ -7,3 +7,13 @@ variable "region" {
   description = "The region Terraform deploys your instance"
   default     = "us-east-2"
 }
+
+variable "is_prod" {
+  description = "The region Terraform deploys your instance"
+  default     = false
+}
+
+variable "vercel_project" {
+  description = "Vercel project ID"
+  default     = "prj_"
+}
