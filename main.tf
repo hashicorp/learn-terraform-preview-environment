20264a3e3f65810dc8075d340dc927dc4c53b75b
Petar Kovacevic <petar.kovacevic@soti.net>
2022-04-25 16:02:43 -0400

    Update Vercel project name and TFC organization name

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   cloud {
     hostname     = "app.terraform.io"
-    organization = "hashicorp-training"
+    organization = "hashiformterra"
     workspaces {
       tags = ["hashicupsBackend"]
     }
