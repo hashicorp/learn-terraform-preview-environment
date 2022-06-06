0c3ff1fd0b26fad414fea5ae002090cc592662a7
Sapeksh Madan <sapmadan@amazon.com>
2022-06-06 15:56:33 -0400

    updated vercel project  org

--- preview-env/main.tf
+++ preview-env/main.tf
@@ -6,7 +6,7 @@ data "terraform_remote_state" "shared" {
   backend = "remote"
 
   config = {
-    organization = "hashicorp-training"
+    organization = "learning-aws-dev"
     workspaces = {
       name = "hcup-be-shared"
     }
