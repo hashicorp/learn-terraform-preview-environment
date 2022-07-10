8904ef66caa4d63d1227f85b5f55a6a7fa11d305
bluemagma612 <bryce@bluemagma.com>
2022-07-10 13:52:29 -0500

    Update Vercel project name and TFC organization name

--- preview-env/main.tf
+++ preview-env/main.tf
@@ -6,7 +6,7 @@ data "terraform_remote_state" "shared" {
   backend = "remote"
 
   config = {
-    organization = "hashicorp-training"
+    organization = "bluemagma"
     workspaces = {
       name = "hcup-be-shared"
     }
