c5b5aa323486aadb792d1a9f0995b5f80db4c292
Mymaks <mymaks@example.com>
2022-07-27 00:07:05 +0300

    Update Vercel project name and TFC organization name

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   cloud {
     hostname     = "app.terraform.io"
-    organization = "hashicorp-training"
+    organization = "Mymaks"
     workspaces {
       tags = ["hashicupsBackend"]
     }
--- preview-env/main.tf
+++ preview-env/main.tf
@@ -6,7 +6,7 @@ data "terraform_remote_state" "shared" {
   backend = "remote"
 
   config = {
-    organization = "hashicorp-training"
+    organization = "Mymaks"
     workspaces = {
       name = "hcup-be-shared"
     }
