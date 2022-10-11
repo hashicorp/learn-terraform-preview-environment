97f9e32f5c7b72c018e38c736f33277eda269e90
techarx <34015628+techarx@users.noreply.github.com>
2022-10-10 22:00:55 -0500

    Update Vercel project name and TFC organization name

--- preview-env/main.tf
+++ preview-env/main.tf
@@ -6,7 +6,7 @@ data "terraform_remote_state" "shared" {
   backend = "remote"
 
   config = {
-    organization = "hashicorp-training"
+    organization = "tfe-whales"
     workspaces = {
       name = "hcup-be-shared"
     }
