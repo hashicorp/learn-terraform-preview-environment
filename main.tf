e4220870d56d7760dba8e5be0a4b74cf8ab69264
Chris McGehee <cmcgehee@datto.com>
2022-11-18 20:53:29 -0800

    update org name

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   cloud {
     hostname     = "app.terraform.io"
-    organization = "hashicorp-training"
+    organization = "risto-mcgehee-personal"
     workspaces {
       tags = ["hashicupsBackend"]
     }
--- preview-env/main.tf
+++ preview-env/main.tf
@@ -6,7 +6,7 @@ data "terraform_remote_state" "shared" {
   backend = "remote"
 
   config = {
-    organization = "hashicorp-training"
+    organization = "risto-mcgehee-personal"
     workspaces = {
       name = "hcup-be-shared"
     }
