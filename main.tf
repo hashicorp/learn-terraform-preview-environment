03451e246d1024beae094e780c73afd60a920e54
rex <rex@101.xyz>
2023-01-23 14:59:53 -0500

    Update Vercel project name and TFC organization name

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   cloud {
     hostname     = "app.terraform.io"
-    organization = "hashicorp-training"
+    organization = "vera-laseine-mortuna"
     workspaces {
       tags = ["hashicupsBackend"]
     }
