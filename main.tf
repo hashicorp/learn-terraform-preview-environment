03f68b74373fcf7b4bb38fc25277357a615378be
stephen <stephen@motts.id.au>
2023-03-06 17:57:41 +0800

    updates org

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   cloud {
     hostname     = "app.terraform.io"
-    organization = "hashicorp-training"
+    organization = "stephenorg"
     workspaces {
       tags = ["hashicupsBackend"]
     }
