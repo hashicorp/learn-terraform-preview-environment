fa429e0e3b6c5b73accc2464f362c87f7d5b2315
jacobbuckles-hc <jacob.buckles@hashicorp.com>
2023-07-06 14:15:10 -0500

    updated orgs

--- preview-env/main.tf
+++ preview-env/main.tf
@@ -6,7 +6,7 @@ data "terraform_remote_state" "shared" {
   backend = "remote"
 
   config = {
-    organization = "hashicorp-training"
+    organization = "jacobbuckles-org"
     workspaces = {
       name = "hcup-shared-resources"
     }
