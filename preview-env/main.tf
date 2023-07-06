4959853bc66d39a0b030e120a87b14b306135e6a
Thomas Österling <42211685+thotte@users.noreply.github.com>
2023-07-06 14:52:32 +0200

    Update Vercel project name and TFC organization name

--- preview-env/main.tf
+++ preview-env/main.tf
@@ -6,7 +6,7 @@ data "terraform_remote_state" "shared" {
   backend = "remote"
 
   config = {
-    organization = "hashicorp-training"
+    organization = "thomas-org"
     workspaces = {
       name = "hcup-be-shared"
     }
