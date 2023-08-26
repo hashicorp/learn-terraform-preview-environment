168ed0b2d70bb8a2a41b10626f416ba42b037416
BlakeFernandes <blakef148@gmail.com>
2023-08-26 21:57:27 +1200

    changed name again

--- preview-env/outputs.tf
+++ preview-env/outputs.tf
@@ -7,5 +7,5 @@ output "public_ip" {
 }
 
 output "preview_url" {
-  value = vercel-deployment.frontend.url
+  value = vercel_deployment.frontend.url
 }
--- preview-env/vercel.tf
+++ preview-env/vercel.tf
@@ -6,7 +6,7 @@ locals {
   public_api_url = length(aws_lb.app) > 0 ? "https://${aws_lb.app[0].dns_name}" : ""
 }
 
-resource "vercel-deployment" "frontend" {
+resource "vercel_deployment" "frontend" {
   project_id = data.terraform_remote_state.shared.outputs.vercel_project_id
   files      = data.vercel_project_directory.frontend.files
   production = var.is_prod
